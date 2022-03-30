2caf64ee7bd6e07bdfd684873a88b4c79b3f52ea
J1min <jmhong101506@gmail.com>
2022-03-30 16:46:36 +0900

    헤더 완성 CSS 다듬기 ㄱㄱ

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -7,32 +7,58 @@ import $ from "jquery";
 import { Navbar, NavDropdown, Container, Nav } from "react-bootstrap";
 export default function main() {
   const Logo = "localhost";
-  $("#toggle").click(function () {
-    $(".lists").toggle(400);
-    $(".log").toggle(400);
-  });
+
+  const toggleBtn = document.querySelector(".navbar__toggleBtn");
+  const menu = document.querySelector(".navbar__menu");
+  const links = document.querySelector(".navbar__links");
+  let status = 0;
+  let click = () => {
+    if (!status) {
+      $(".navbar__menu").addClass("active");
+      $(".navbar__links").addClass("active");
+      status = 1;
+    } else {
+      $(".navbar__menu").removeClass("active");
+      $(".navbar__links").removeClass("active");
+      status = 0;
+    }
+  };
+
   return (
-    <section id="header">
-      <div className="wrapper">
-        <S.MainText className="title">{Logo}</S.MainText>
-        <ul className="lists">
-          <S.Nav>
-            <Link to="community">커뮤니티</Link>
-          </S.Nav>
-          <S.Nav>
-            <Link to="community">커뮤니티</Link>
-          </S.Nav>
-          <S.Nav>
-            <Link to="community">커뮤니티</Link>
-          </S.Nav>
-          <S.Nav>
-            <Link to="community">커뮤니티</Link>
-          </S.Nav>
-        </ul>
-        <S.Nav className="log">
-          <Link to="login">Log In</Link>
-        </S.Nav>
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <i className="fab fa-apple"></i>
+        <a href="">민브로 코딩</a>
       </div>
-    </section>
+      <ul className="navbar__menu">
+        <li>
+          <a href="">홈</a>
+        </li>
+        <li>
+          <a href="">핫딜</a>
+        </li>
+        <li>
+          <a href="">포럼</a>
+        </li>
+        <li>
+          <a href="">FAQ</a>
+        </li>
+        <li>
+          <a href="">채용</a>
+        </li>
+      </ul>
+      <ul className="navbar__links">
+        <li>
+          <i className="fab fa-facebook-square">페북</i>
+        </li>
+        <li>
+          <i className="fab fa-instagram">인스타</i>
+        </li>
+      </ul>
+
+      <a href="#" className="navbar__toggleBtn" onClick={click}>
+        <i className="fas fa-bars">함박</i>
+      </a>
+    </nav>
   );
 }
