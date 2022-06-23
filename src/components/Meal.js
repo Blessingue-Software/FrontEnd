91e69a9f1be27b4c608dd43f171943d93cf9d081
J1min <jmhong101506@gmail.com>
2022-06-23 23:24:14 +0900

    FIX :: 버튼 디자인 변경

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -2,24 +2,22 @@ import React from "react";
 import axios from "axios";
 import dayjs from "dayjs";
 import "./Meal.scss";
-
+import { useState } from "react";
+import * as S from "./Style";
 export default function SchoolLunch() {
   const $ = document.querySelector.bind(document);
   const SCHOOLCODE = 7150658;
   const LOCALCODE = "C10";
-
   const TODAY = dayjs().format("YYYYMMDD"); // 20220623
   let date = parseInt(TODAY);
-
   let URL = `https://open.neis.go.kr/hub/mealServiceDietInfo?&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=${LOCALCODE}&SD_SCHUL_CODE=${SCHOOLCODE}&MLSV_YMD=${date}`;
-
   let today = new Date().getDay();
-
   function getTodayLabel() {
     let week = ["일", "월", "화", "수", "목", "금", "토"];
     let todayLabel = week[today % 7];
     return todayLabel;
   }
+  let [showDate, setShowDate] = useState(getTodayLabel());
   function loadMeal(response) {
     if (getTodayLabel() === "금") {
       const breakfastMenu =
@@ -55,8 +53,6 @@ export default function SchoolLunch() {
     date++;
     today++;
     getTodayLabel();
-    console.log(date);
-    console.log(getTodayLabel());
     URL = `https://open.neis.go.kr/hub/mealServiceDietInfo?&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=${LOCALCODE}&SD_SCHUL_CODE=${SCHOOLCODE}&MLSV_YMD=${date}`;
     axios.get(URL).then((response) => {
       loadMeal(response);
@@ -64,8 +60,8 @@ export default function SchoolLunch() {
   }
   return (
     <div className="meal-container">
-      {date}
-      <button onClick={upDate}>+</button>
+      날짜 : {showDate}
+      <S.calendarBtn onClick={upDate}>+</S.calendarBtn>
       <div className="breakfast-container">
         <div className="breakfast-title">조식</div>
         <div className="breakfast"></div>
