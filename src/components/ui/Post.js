90786328d4ab516048c1188bb52fbbe60b29de9d
J1min <jmhong101506@gmail.com>
2022-07-12 09:43:36 +0900

    ADD :: Scss 추가

--- src/components/ui/Post.js
+++ src/components/ui/Post.js
@@ -1,34 +1,32 @@
 import React from "react";
 import * as S from "../Style";
-export default function Text() {
-  return (
-    <S.Post>
-      <S.PostList>
-        디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
-      </S.PostList>
-      <S.PostList>
-        디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
-      </S.PostList>
-      <S.PostList>
-        디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
-      </S.PostList>
-      <S.PostList>
-        디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
-      </S.PostList>
-      <S.PostList>
-        디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
-      </S.PostList>
-      <S.PostList>
-        디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
-      </S.PostList>
-      <S.PostList>
-        디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
-      </S.PostList>
-      <S.PostList>
-        디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
-      </S.PostList>
-      
-
-    </S.Post>
-  );
+export default function Post() {
+    return (
+        <S.Post>
+            <S.PostList>
+                디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
+            </S.PostList>
+            <S.PostList>
+                디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
+            </S.PostList>
+            <S.PostList>
+                디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
+            </S.PostList>
+            <S.PostList>
+                디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
+            </S.PostList>
+            <S.PostList>
+                디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
+            </S.PostList>
+            <S.PostList>
+                디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
+            </S.PostList>
+            <S.PostList>
+                디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
+            </S.PostList>
+            <S.PostList>
+                디자이너, 프론트 하실분 있으신가요?? 2216 홍지민 문의바랍니다
+            </S.PostList>
+        </S.Post>
+    );
 }
--- src/components/ui/Project.js
+++ src/components/ui/Project.js
@@ -1,18 +1,16 @@
 /* eslint-disable */
 import React from "react";
 import * as S from "../Style";
-import "./Main.scss"
-export default function Page() {
-  return (
-    <S.Squre id="disabled">
-      <span>
-        디자이너
-        <br />
-        프론트
-      </span>
-      <S.Num>
-        구인중
-      </S.Num>
-    </S.Squre>
-  );
+import "./Project.scss";
+export default function Project() {
+    return (
+        <S.Squre id="disabled">
+            <span>
+                디자이너
+                <br />
+                프론트
+            </span>
+            <S.Num>구인중</S.Num>
+        </S.Squre>
+    );
 }
