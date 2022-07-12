26844cc5bd9860b4fd28b2a7b39e4db93e1f22b2
J1min <jmhong101506@gmail.com>
2022-07-13 07:21:19 +0900

    ADD :: @babel/runtime (빌드이슈 해결)

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper } from "swiper/react";
+import { SwiperSlide } from "swiper/react";
 import Banner from "../img/배포용-배너이미지.png";
 import "swiper/css";
 import "../../index.css";
@@ -11,29 +12,29 @@ import "swiper/css/autoplay";
 import "./Slider.scss";
 
 export default function Silder() {
-  return (
-    <>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, Autoplay]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        // scrollbar={{ draggable: true }}
-        // onSlideChange={() => console.log("slide change")}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-      >
-        {[1, 2, 3].map((a, i) => {
-          return (
-            <SwiperSlide key={i}>
-              <img src={Banner} alt="1  " className="slide" />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </>
-  );
+    return (
+        <>
+            <Swiper
+                modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+                spaceBetween={20}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                // scrollbar={{ draggable: true }}
+                // onSlideChange={() => console.log("slide change")}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+            >
+                {[1, 2, 3].map((a, i) => {
+                    return (
+                        <SwiperSlide key={i}>
+                            <img src={Banner} alt="1  " className="slide" />
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+        </>
+    );
 }
